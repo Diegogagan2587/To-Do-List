ddffc778e335629a2c3fe9c6cff74457abe6fedb
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-07 10:52:04 -0600

    add function to display tasks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,60 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+console.log('index.js is working!')
+
+function Task (
+  description,
+  completed,
+  index
+) {
+  this.description = description;
+  this.completed = completed;
+  this.index = index;
+}
+
+const toDoList = [
+  new Task (
+    'clean the bathroom',
+    false,
+    0
+  ),
+  new Task (
+    'make the bed',
+    true,
+    1
+  ),
+  new Task (
+    'wash the dishes',
+    false,
+    2
+  ),
+  new Task (
+    'clean the city',
+    false,
+    3
+  )
+]
+
+/* Start display to do list */
+function display(toDoList) {
+  const listContainer = document.querySelector('#list-container');
+  listContainer.innerHTML = '';
+  let index = 0;
+  toDoList.forEach(task => {
+    const liElem = document.createElement('li');
+    const description = task.description;
+    if(task.completed) {
+      liElem.innerHTML = `<div class="square" value="${index}">&#10003;</div><p>${description}</p>`;
+    } else {
+      liElem.innerHTML = `<div class="square" value="${index}"></div><p>${description}</p>`; 
+    }
+    listContainer.appendChild(liElem);
+    index++;
+  });
+
+}
+display(toDoList);
+/* finish display to do list */
+
+
+
