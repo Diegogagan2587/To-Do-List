5f4907b6375f8a599635467fd50379eb18df9faf
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-07 15:58:54 -0600

    create task_constructor within a module

---
+++ src/modules/task_constructor.js
@@ -0,0 +1,11 @@
+function Task (
+    description,
+    completed,
+    index
+  ) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+
+  export default Task;
