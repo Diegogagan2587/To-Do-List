79abdff8588b3011bfd0628441c6b4a0522882eb
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-07 14:20:37 -0600

    set test: png,svg,jpg,jpeg,gif for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template:'src/index.html',
+        template:'./src/index.html',
     })
   ],
   output: {
@@ -25,6 +25,14 @@ module.exports = {
       {
         test:/\.css$/i,
         use: [ 'style-loader','css-loader' ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
