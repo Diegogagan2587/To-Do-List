1516672334a6ca42936cba00d56a66f1cc51fff2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-08 20:01:12 -0600

    create a method within amodule to handle status updates ture or false

--- src/modules/task_status.js
+++ src/modules/task_status.js
@@ -1,5 +1,18 @@
-function test () {
-    return test = 'test';
+import toDoList from "./toDoList_data.js";
+
+const statusHandler = {
+    change (event) {
+        const currentCheckBox = event.target;
+        const currentTaskDescription = event.target.parentNode.querySelector('.current-task-input');
+        const currentTaskIndex = event.target.value;
+        currentCheckBox.classList.toggle('checked');
+        currentTaskDescription.classList.toggle('checked-text')
+        if(currentCheckBox.classList.contains('checked')) {
+            toDoList[currentTaskIndex].completed = true;
+        } else {
+            toDoList[currentTaskIndex].completed = false;
+        }
+    }
 }
 
-export default test;
+export default statusHandler;
