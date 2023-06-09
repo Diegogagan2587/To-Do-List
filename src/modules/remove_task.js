5f0a2f825543757fe82937e0fe3cc785313fb85d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-08 22:24:31 -0600

    fix erro: after removing 1 of 2 items if click over checkbox now will update localStorage

--- src/modules/remove_task.js
+++ src/modules/remove_task.js
@@ -1,6 +1,7 @@
 import toDoList from './toDoList_data.js';
 import display from './display.js';
 import updateLocalStorage from './update_local_sorage.js';
+import upDateListeners from './update_listeners.js';
 
 function removeTask(event) {
   const indexToRemove = event.target.parentNode.value;
@@ -14,9 +15,12 @@ function removeTask(event) {
   updateLocalStorage(toDoList);
 
   /* -- Now we Re-set the listeners since display clean all -- */
+  upDateListeners();
+
+  /*
   const allDeleteButton = document.querySelectorAll('.delete-button');
   allDeleteButton.forEach((button) => {
     button.addEventListener('click', removeTask);
-  });
+  });*/
 }
 export default removeTask;
